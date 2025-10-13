ffc3162a8f8bf600a8121b6d5921fae80da99d8b
DynamicAdi <adarshpanditdev@gmail.com>
2025-10-14 02:03:21 +0530

    sanity backend schema done/

---
+++ sanity/schemaTypes/Portfolio.ts
@@ -0,0 +1,52 @@
+import { DocumentTextIcon } from "@sanity/icons";
+import { defineField, defineType } from "sanity";
+
+export const portfolioType = defineType({
+  name: "portfolio",
+  title: "Our Portfolio",
+  type: "document",
+  icon: DocumentTextIcon,
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+      },
+    }),
+    defineField({
+      name: "category",
+      type: "string",
+    }),
+    defineField({
+      name: "hostedUrl",
+      type: "string"
+    }),
+    defineField({
+      name: "mainImage",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        defineField({
+          name: "alt",
+          type: "string",
+          title: "Alternative text",
+        }),
+      ],
+    }),
+    defineField({
+      name: "publishedAt",
+      type: "datetime",
+    }),
+    defineField({
+      name: "body",
+      type: "blockContent",
+    }),
+  ]
+});
--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -4,7 +4,9 @@ import {blockContentType} from './blockContentType'
 import {postType} from './postType'
 import {authorType} from './authorType'
 import { caseType } from './caseStudy'
+import { ProductType } from './OwnProducts'
+import { portfolioType } from './Portfolio'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, postType, authorType, caseType],
+  types: [blockContentType, postType, authorType, caseType, ProductType, portfolioType],
 }
